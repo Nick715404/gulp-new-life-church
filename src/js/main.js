6a3cf74332aa4daca37a65229f30fe40f8e69a8b
nicklllson <nikita.vasia2015@gmail.com>
2023-10-17 09:37:31 +0500

    Almost done with first page

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
+  slidesPerView: 2,
+  spaceBetween: 25,
 
   // Navigation arrows
   navigation: {
