96abef2d0d0b23a599bd08f1ac54ff00ebc5ef16
nicklllson <nikita.vasia2015@gmail.com>
2023-12-08 02:55:39 +0500

    Добавил секцию со служениями

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,11 @@
 @@include('./libs/swiper-bundle.min.js');
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.sermons-swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
   slidesPerView: 2,
   spaceBetween: 25,
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
